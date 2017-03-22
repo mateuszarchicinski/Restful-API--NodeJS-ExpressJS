0562ff68e079b5aaf9fc63fb3f12c10d7c9b6115
mateuszarchicinski <mateusz.archicinski@gmail.com>
2017-03-23 00:17:53 +0100

    Implemented useful functions to handles all possible routes & Added new options in project.config.js

--- app.js
+++ app.js
@@ -8,11 +8,151 @@
 const express = require('express'),
       cors = require('cors'),
       bodyParser = require('body-parser'),
-      compression = require('compression');
+      compression = require('compression'),
+      chalk = require('chalk');
 
 
 // PROJECT CONFIG
 const PROJECT_CONFIG = require('./config/project.config.js');
+
+
+// USEFUL FUNCTIONS
+// To display a console log message in four types: success, info, warning and error
+function alertHandler (args) {
+    
+    args = args || {};
+    
+    let types = {
+        success: 'green',
+        info: 'blue',
+        warning: 'yellow',
+        error: 'red'
+    },
+        type = args.type || 'info',
+        title = args.title || type,
+        message = args.message || 'Remember to specify necessary property type & message in a configuration object.',
+        color = types[type],
+        messageTemplate = `
+**~~~~~~~~* ${title.toUpperCase()} LOG - OPEN *~~~~~~~~**
+${message}
+**~~~~~~~~* ${title.toUpperCase()} LOG - CLOSE *~~~~~~~~**`;
+    
+    console.log(chalk[color](messageTemplate));
+    
+};
+
+
+// To create full URL address as a value (string) or false (boolean)
+function createFullUrl (lang, url) {
+    
+    return lang && url ? `/${lang}/${url}` : false;
+    
+};
+
+
+// To create file full name as a value (string) or false (boolean)
+function createFileFullName (name, lang) {
+    
+    return name && lang ? `${name}-${lang}.html` : false;
+    
+};
+
+
+// To create redirect page as a value (object)
+function createPageRedirect (args) {
+    
+    args = args || {};
+    
+    let self = {
+        statusCode: args.statusCode || PROJECT_CONFIG.HTTP_CODE.REDIRECT,
+        redirect: {
+            url: args.url || createFullUrl(PROJECT_CONFIG.LANGUAGES[0], getPage( {param: 'name', value: '404 page'} ).url)
+        }
+    };
+    
+    return self;
+    
+};
+
+
+// To get supported language as a value (string) or false (boolean)
+function getLang (arg) {
+    
+    return PROJECT_CONFIG.LANGUAGES.includes(arg) ? arg : false;
+    
+};
+
+
+// To get existed page as a value (object) or false (boolean)
+function getPage (args) {
+    
+    args = args || {};
+    
+    if (!args.value) {
+        return false;
+    }
+    
+    let searchTypes = ['name', 'url'],
+        searchParam = searchTypes.includes(args.param) ? args.param : 'name',
+        searchValue = args.value,
+        pages = PROJECT_CONFIG.PAGES;
+    
+    for (let i in pages) {
+        if (pages[i][searchParam] === searchValue) {
+            pages[i].statusCode = pages[i].statusCode || PROJECT_CONFIG.HTTP_CODE.SUCCESS;
+            pages[i].root = pages[i].root || PROJECT_CONFIG.DIRECTORY.PAGES_DIR;
+            pages[i].redirect = pages[i].redirect || false;
+            
+            return pages[i];
+        }
+    }
+    
+    return false;
+    
+};
+
+
+// To handle all possible routes, returns value (object) with properties of an appropriate page or a redirect page
+function routeHandler (params) {
+    
+    params = params || {};
+    
+    
+    let langValue = getLang(params.lang);
+    
+    
+    if (!PROJECT_CONFIG.MAIN_PAGE.IS_URL && langValue && !params.page && !params[0]) {
+        params.page = '/';
+    }
+    
+    let pageObj = getPage( {param: 'url', value: params.page} );
+    
+    if (pageObj) {
+        pageObj.fileFullName = createFileFullName(pageObj.fileName, langValue || PROJECT_CONFIG.LANGUAGES[0]);
+    }
+    
+    
+    // Redirects in these cases to Main Page
+    if (!pageObj && (((!PROJECT_CONFIG.MAIN_PAGE.IS_URL || !langValue) && !params[0]) || ((PROJECT_CONFIG.MAIN_PAGE.IS_URL || langValue) && !params[0]) || (!langValue && params[0]))) {
+        let mainPageUrl = getPage( {param: 'name', value: PROJECT_CONFIG.MAIN_PAGE.NAME} ).url;
+        
+        return pageObj = createPageRedirect({
+            url: `/${langValue || PROJECT_CONFIG.LANGUAGES[0]}${mainPageUrl === '/' ? '' : '/' + mainPageUrl}`
+        });
+    }
+    
+    
+    // Redirects in these cases to 404 Page
+    if (!langValue || !pageObj || params[0]) {
+        pageObj = createPageRedirect({
+            url: createFullUrl(langValue || PROJECT_CONFIG.LANGUAGES[0], getPage( {param: 'name', value: PROJECT_CONFIG[404].NAME} ).url)
+        });
+    }    
+    
+    
+    return pageObj;
+    
+};
 
 
 // INIT EXPRESS APP ---> https://expressjs.com/en/4x/api.html
@@ -33,6 +173,10 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
 
+// Compresses response bodies for all request, more info about COMPRESSION middleware ---> https://github.com/expressjs/compression#compression
+app.use(compression({threshold: 0}));
+
+
 // Redirects all insecure requests to secure protocol HTTPS only in production environment
 /*
 if (app.get('env') === 'production') {
@@ -47,28 +191,68 @@ if (app.get('env') === 'production') {
 */
 
 
-// Compresses response bodies for all request, more info about COMPRESSION middleware ---> https://github.com/expressjs/compression#compression
-app.use(compression({threshold: 0}));
-
-
 // Serves static files from the directory, which is defined in the project.config.js file
 app.use('/', express.static(PROJECT_CONFIG.DIRECTORY.STATIC_DIR));
 
 
-// Simple routes to send the appropriate index file
-app.get(['/', '/:lang', '/:lang/*'], function (req, res) {
+////////////////////////////////////
+//                                //
+//          *** OPEN ***          //
+//  * HERE ADD ALL YOUR ROUTES *  //
+//                                //
+////////////////////////////////////
+
+
+//
+
+
+////////////////////////////////////
+//                                //
+//  * HERE ADD ALL YOUR ROUTES *  //
+//         *** CLOSE ***          //
+//                                //
+////////////////////////////////////
+
+
+// Handles all possible routes to send the appropriate HTML file
+app.get(['/', '/:lang', '/:lang/:page', '/:lang/:page/*', '*'], function (req, res, next) {
     
-    var value = req.params.lang,
-        lang = PROJECT_CONFIG.LANGUAGES.includes(value) ? value : PROJECT_CONFIG.LANGUAGES[0],
-        index = 'index-' + lang + '.html';
+    var options = routeHandler(req.params);
     
     
-    res.setHeader('Content-Type', 'text/html');
+    if (options.redirect) {
+        return res.redirect(options.statusCode, options.redirect.url);
+    }
     
-    res.sendFile(index, {
+    res.status(options.statusCode).type('html').sendFile(options.fileFullName, {
         maxAge: 86400000,
-        root: PROJECT_CONFIG.DIRECTORY.STATIC_DIR
+        root: options.root
     });
+    
+});
+
+
+// Handles HTTP errors
+app.use(function (err, req, res, next) {
+    
+    alertHandler({
+        type: 'error',
+        message: err
+    });
+    
+    alertHandler({
+        type: 'error',
+        message: err.stack
+    });
+    
+    
+    let statusCode = PROJECT_CONFIG.HTTP_CODE.SUPPORTED_ERRORS.includes(err.statusCode) ? err.statusCode : PROJECT_CONFIG.HTTP_CODE.SUPPORTED_ERRORS[0],
+        options = createPageRedirect({
+            url: createFullUrl(getLang(req.params.lang) || PROJECT_CONFIG.LANGUAGES[0], getPage( {param: 'name', value: PROJECT_CONFIG[statusCode].NAME} ).url)
+        });
+    
+    
+    res.redirect(options.statusCode, options.redirect.url);
     
 });
 
--- config/project.config.js
+++ config/project.config.js
@@ -1,12 +1,67 @@
 // PROJECT CONFIG
 module.exports = {
-    LANGUAGES: [ // Remember to rename your index files as index-pl.html & index-en.html
+    MODE: 'Normal', // Normal / Angular - atm works only Normal mode
+    LANGUAGES: [ // Add to each HTML file a language suffix e.g index-en.html etc.
         'pl', // First element is default variable
         'en'
     ],
+    MAIN_PAGE: {
+        NAME: 'dashboard page',
+        IS_URL: true
+    },
+    404: {
+        NAME: '404 page'
+    },
+    500: {
+        NAME: '500 page'
+    },
+    PAGES: [ // All types of pages need to be defined in this array
+        {
+            name: 'main page',
+            url: '/',
+            fileName: 'index',
+            type: 'main',
+            root: __dirname + '/../public/'
+        },
+        {
+            name: 'dashboard page',
+            url: 'dashboard',
+            fileName: 'dashboard',
+            type: 'normal'
+        },
+        {
+            name: 'dashboard page2',
+            url: 'dashboard2',
+            fileName: 'dashboard2',
+            type: 'normal'
+        },
+        {
+            name: '404 page',
+            url: '404',
+            statusCode: 404,
+            fileName: '404',
+            type: '404'
+        },
+        {
+            name: '500 page',
+            url: '500',
+            statusCode: 500,
+            fileName: '500',
+            type: '500'
+        }
+    ],
+    HTTP_CODE: {
+        SUCCESS: 200,
+        REDIRECT: 301,
+        SUPPORTED_ERRORS: [
+            500,
+            404
+        ]
+    },
     CONFIG_FILE: 'project.config.js',
     DATA_FILE: 'project.data.json',
     DIRECTORY: {
-        STATIC_DIR: __dirname + '/../public/' // Static files directory
+        STATIC_DIR: __dirname + '/../public/', // Static files directory
+        PAGES_DIR: __dirname + '/../public/pages/'
     }
 };
